bb79aab3400175613a93f06b68ed302c6aeca3a8
Christian Schrötter <cs@fnx.li>
2021-04-06 08:20:52 +0200

    crackling: gps: Fix build error after first run

--- gps/Android.mk
+++ gps/Android.mk
@@ -22,6 +22,7 @@ gps_debug_conf := gps_debug.conf
 gps_conf := gps.conf
 
 $(gps_conf_dir)/$(gps_debug_conf):
+		rm -f $(TARGET_OUT_ETC)/$(gps_debug_conf)
 		ln -s $(gps_conf) $(TARGET_OUT_ETC)/$(gps_debug_conf)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(gps_conf_dir)/$(gps_debug_conf)
